16c5b24b1edb34d7b039a74c2b053764707ed261
Anirudh r k <menonani7@gmail.com>
2024-01-28 09:37:42 +0530

    filter and api fetch for images done

--- art-marketplace/frontend/src/Art.js
+++ art-marketplace/frontend/src/Art.js
@@ -1,15 +1,22 @@
 import Footer from "./Footer";
 import Nav from "./Nav";
 import classes from "./css/art.module.css";
-import art from "./images/home-art.jpeg";
-import art1 from './images/art1.jpeg';
-import art2 from './images/art2.jpeg';
-import art3 from './images/art3.jpeg';
-import art4 from './images/art4.jpeg';
-import art5 from './images/art5.jpeg';
+import axios from "axios";
 import { CiSearch } from "react-icons/ci";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
 function Art() {
+  const [data, setData] = useState([]);
+  const[search,setSearch]=useState('');
+  console.log(search);
+  useEffect(() => {
+    axios
+    //replace this with your fetch all api
+      .get("https://picsum.photos/v2/list?page=2&limit=10")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className={classes.head}>
       <Nav />
@@ -22,6 +29,7 @@ function Art() {
           className={classes.searchbar}
           name="search"
           id="search"
+          onChange={(e)=>setSearch(e.target.value)}
           // Add your input onChange handler here to capture search input
         />
         <button type="submit" className={classes.iconMarket}>
@@ -29,68 +37,22 @@ function Art() {
         </button>
       </form>
       <div className={classes.content}>
-        <div className={classes.card}>
-          <Link to="/artPage" style={{color:"white"}}>
-            <img src={art} alt="Avatar" />
-            <div className={classes.container}>
-              <h4>
-                <b style={{ fontSize: "30px"}}>John Doe</b>
-              </h4>
-              <p style={{ fontSize: "20px" }}>Architect & Engineer</p>
-              <p>price: 10$</p>
-            </div>
-          </Link>
-        </div>
-        <div className={classes.card}>
-          <img src={art5} alt="Avatar" />
-          <div className={classes.container}>
-            <h4>
-              <b style={{ fontSize: "30px" }}>John Doe</b>
-            </h4>
-            <p style={{ fontSize: "20px" }}>Architect & Engineer</p>
-            <p>price: 10$</p>
+        {data.filter((item)=>{
+          return search.toLowerCase()===""?item:item.author.toLowerCase().includes(search);
+        }).map((user, index) => (
+          <div className={classes.card} key={index}>
+            <Link to="/artPage" style={{ color: "white" }}>
+              <img src={user.download_url} alt={`Artwork by ${user.author}`} />
+              <div className={classes.container}>
+                <h4>
+                  <b style={{ fontSize: "30px" }}>{user.author}</b>
+                </h4>
+                <p style={{ fontSize: "20px" }}>Architect & Engineer</p>
+                <p>Price: 10$</p>
+              </div>
+            </Link>
           </div>
-        </div>
-        <div className={classes.card}>
-          <img src={art1} alt="Avatar" />
-          <div className={classes.container}>
-            <h4>
-              <b style={{ fontSize: "30px" }}>John Doe</b>
-            </h4>
-            <p style={{ fontSize: "20px" }}>Architect & Engineer</p>
-            <p>price: 10$</p>
-          </div>
-        </div>
-        <div className={classes.card}>
-          <img src={art2} alt="Avatar" />
-          <div className={classes.container}>
-            <h4>
-              <b style={{ fontSize: "30px" }}>John Doe</b>
-            </h4>
-            <p style={{ fontSize: "20px" }}>Architect & Engineer</p>
-            <p>price: 10$</p>
-          </div>
-        </div>
-        <div className={classes.card}>
-          <img src={art3} alt="Avatar" />
-          <div className={classes.container}>
-            <h4>
-              <b style={{ fontSize: "30px" }}>John Doe</b>
-            </h4>
-            <p style={{ fontSize: "20px" }}>Architect & Engineer</p>
-            <p>price: 10$</p>
-          </div>
-        </div>
-        <div className={classes.card}>
-          <img src={art4} alt="Avatar" />
-          <div className={classes.container}>
-            <h4>
-              <b style={{ fontSize: "30px" }}>John Doe</b>
-            </h4>
-            <p style={{ fontSize: "20px" }}>Architect & Engineer</p>
-            <p>price: 10$</p>
-          </div>
-        </div>
+        ))}
       </div>
       <Footer />
     </div>
